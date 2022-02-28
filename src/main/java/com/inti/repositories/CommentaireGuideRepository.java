13b979c6ebca45ca0ee5658eb998b64689b90d99
ShanaBoureux <shana.boureux@gmail.com>
2022-02-28 17:16:14 +0100

    lol

--- src/main/java/com/inti/repositories/CommentaireGuideRepository.java
+++ src/main/java/com/inti/repositories/CommentaireGuideRepository.java
@@ -1,9 +1,10 @@
 package com.inti.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.inti.entities.CommentaireGuide;
-
+@Repository
 public interface CommentaireGuideRepository extends JpaRepository<CommentaireGuide, Long>{
 
 }
