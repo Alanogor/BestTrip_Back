779c30e6627a6c844b1a64f97621e49c5746e987
Martineau31 <dorian.martineau31@gmail.com>
2022-03-01 09:59:18 +0100

    creation des controller

--- src/main/java/com/inti/controller/CommentaireExperienceController.java
+++ src/main/java/com/inti/controller/CommentaireExperienceController.java
@@ -3,13 +3,21 @@ package com.inti.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.inti.entities.CommentaireExperience;
 import com.inti.service.impl.CommentaireExperienceService;
 
 @RestController
+@CrossOrigin
 public class CommentaireExperienceController {
 	@Autowired
 	CommentaireExperienceService commentaireExperienceService;
@@ -17,5 +25,26 @@ public class CommentaireExperienceController {
 	@GetMapping("/commentaire_experience")
 	public List<CommentaireExperience> findAll() {
 		return commentaireExperienceService.findAll();
+	}
+	
+	@GetMapping("/commentaire_experience/{id}")
+	public CommentaireExperience findOne(@PathVariable("id") Long id) {
+		return commentaireExperienceService.findOne(id);
+	}
+	
+	@DeleteMapping("/commentaire_experience/{id}")
+	public void delete(@PathVariable("id") Long id) {
+		commentaireExperienceService.delete(id);
+	}
+	
+	@PostMapping("/commentaire_experience")
+	public CommentaireExperience save(@RequestBody CommentaireExperience obj) {
+		return commentaireExperienceService.save(obj);
+	}
+	
+	@PutMapping("/commentaire_experience/{id}")
+	public CommentaireExperience update(@RequestBody CommentaireExperience obj,@PathVariable("id") Long id) {
+		obj.setIdComExperience(id);
+		return commentaireExperienceService.save(obj);
 	}
 }
--- src/main/java/com/inti/controller/ExperienceController.java
+++ src/main/java/com/inti/controller/ExperienceController.java
@@ -1,8 +1,50 @@
 package com.inti.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class ExperienceController {
+import com.inti.entities.Experience;
+import com.inti.service.impl.ExperienceService;
 
+@RestController
+@CrossOrigin
+public class ExperienceController {
+	@Autowired
+	ExperienceService experienceService;
+	
+	@GetMapping("/experience")
+	public List<Experience> findAll() {
+		return experienceService.findAll();
+	}
+	
+	@GetMapping("/experience/{id}")
+	public Experience findOne(@PathVariable("id")Long id) {
+		return experienceService.findOne(id);
+	}
+	
+	@PostMapping("/experience")
+	public Experience save(@RequestBody Experience obj) {
+		return experienceService.save(obj);
+	}
+	
+	@DeleteMapping("/experience/{id}")
+	public void delete(@PathVariable("id")Long id) {
+		experienceService.delete(id);
+	}
+	
+	@PutMapping("/experience/{id}")
+	public Experience update(@PathVariable("id")Long id,@RequestBody Experience obj) {
+		obj.setIdExperience(id);
+		return experienceService.save(obj);
+	}
 }
