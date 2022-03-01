f808f8e638e7dcb8be79a823beea33973a45ad20
Anthony Jougla <anthony.jougla@gmail.com>
2022-03-01 17:23:01 +0100

    securite

--- src/main/java/com/inti/GestionBestTripApplication.java
+++ src/main/java/com/inti/GestionBestTripApplication.java
@@ -2,12 +2,19 @@ package com.inti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class GestionBestTripApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionBestTripApplication.class, args);
+	}
+	
+	@Bean // pour pouvoir injecter l'instance de la classe BCryptPasswordEncoder dans le conteneur Spring Context
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(); 
 	}
 
 }
