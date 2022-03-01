5924c1e6b158c47adfbce036dc41a128cd80b632
Anthony Jougla <anthony.jougla@gmail.com>
2022-03-01 10:26:21 +0100

    controller role et user

--- src/main/java/com/inti/controller/RecommandationController.java
+++ src/main/java/com/inti/controller/RecommandationController.java
@@ -1,17 +1,13 @@
 package com.inti.controller;
 
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class RecommandationController {
-=======
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
->>>>>>> 4185006dc87e833d530c4553c30fcfdc4068c12f
 
 import com.inti.service.interfaces.IRecommandationService;
+
 @RestController
 @CrossOrigin
 public class RecommandationController {
--- src/main/java/com/inti/controller/RoleController.java
+++ src/main/java/com/inti/controller/RoleController.java
@@ -17,39 +17,34 @@ import org.springframework.web.bind.annotation.RestController;
 import com.inti.entities.Role;
 import com.inti.service.interfaces.IRoleService;
 
-@RestController // RestFul WS (http: post, put, get et delete)
+@RestController 
 @CrossOrigin
 public class RoleController {
-	@Autowired // IRoleService roleService=new RoleService(); => @Service
+	@Autowired 
 	IRoleService roleService;
 
-	// @RequestMapping(value="roles",method=RequestMethod.GET) //
-	// localhost:9090/roles
-	@GetMapping("roles") // localhost:9090/roles
+	
+	@GetMapping("roles") 
 	public List<Role> findAll() {
 		return roleService.findAll();
 	}
 
-	@GetMapping("roles/{idR}") // localhost:9090/roles/6
-	// @RequestMapping(value="roles/{idR}",method=RequestMethod.GET)
-	public Role findOne(@PathVariable("idR") Long idRole) { // (idRole = 6)
-		return roleService.findOne(idRole); // roleService.findOne(6)
+	@GetMapping("roles/{idR}") 
+	public Role findOne(@PathVariable("idR") Long idRole) { 
+		return roleService.findOne(idRole);
 	}
 
-	@PostMapping("roles") // localhost:9090/roles method: post => Les données (le body de la requête http)
-	// @RequestMapping(value="roles",method=RequestMethod.POST)
+	@PostMapping("roles") 
 	public Role saveRole(@RequestBody Role role) {
 		return roleService.save(role);
 	}
 
 	@DeleteMapping("roles/{idRole}")
-	// @RequestMapping(value="roles/{idRole}",method=RequestMethod.DELETE)
 	public void deleteRole(@PathVariable("idRole") Long idRole) {
 		roleService.delete(idRole);
 	}
 
 	@PutMapping("roles/{idR}")
-	//@RequestMapping(value="roles/{idR}",method = RequestMethod.PUT)
 	public Role updateRole(@PathVariable("idR") Long idRole, @RequestBody Role role) {
 		Role currentRole = roleService.findOne(idRole);
 		currentRole.setLibelle(role.getLibelle());
